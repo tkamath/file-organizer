bf8164ef55f9e20f4caa902923a1ee10054c9d36
tkamath <garnetmelodies@gmail.com>
2014-12-29 23:57:53 -0800

    completed version

--- src/filefinder/Main.java
+++ src/filefinder/Main.java
@@ -1,47 +1,119 @@
 package filefinder;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.OutputStreamWriter;
 import java.nio.file.DirectoryIteratorException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
-public class Main {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.WindowConstants;
 
+public class Parser {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str = "C:\\Users\\TK2012\\Desktop\\New folder";
-		Path path = FileSystems.getDefault().getPath(str);
 		
-		String newstr = path.getParent().toString() + "\\New list";
-		Path newdir = FileSystems.getDefault().getPath(newstr);
-		try {
-			Files.createDirectory(newdir);
-		} catch (IOException e) {
-			System.err.println(e.getMessage());
-		}
+		// TODO Auto-generated method stub
+		JFrame frame = new JFrame("Music File Organizer");
+		JButton button = new JButton("Click here!");
+		button.setFont(new Font("Sans Serif", Font.BOLD, 20));
+		button.addActionListener(new ActionListener() {
 
-		helper(path, newdir);
-	}
-
-	static void helper(Path path, Path folder) {
-		try {
-			DirectoryStream<Path> stream = Files.newDirectoryStream(path);
-			for (Path file: stream) {
-				if (!Files.isDirectory(file)) {
-
-					int start = file.toString().lastIndexOf("\\");
-					String newname = folder.toString() + "\\" + file.toString().substring(start + 1);
-
-					Files.copy(file, FileSystems.getDefault().getPath(newname));
-					return;
-				} else {
-					helper(file, folder);
-				}
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				String msgToUsers = "Enter source path here";
+				String boxTitle = "Music File Organizer";
+				JOptionPane genOpPane = new JOptionPane();
+				Object result = genOpPane.showInputDialog(null, msgToUsers, boxTitle, JOptionPane.QUESTION_MESSAGE, null, null, null);
+				arrangeFiles((String) result);
 			}
-		} catch (IOException | DirectoryIteratorException e) {
-			System.err.println(e.getMessage());
+		});
+		
+		JButton stop = new JButton("Exit");
+		stop.setFont(new Font("Sans Serif", Font.BOLD, 12));
+		stop.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				System.exit(0);
+			}
+		});
+		
+		frame.setLayout(new BorderLayout());
+		frame.add(button, BorderLayout.NORTH);
+		frame.add(stop, BorderLayout.SOUTH);
+		frame.setSize(400, 100);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+	}
+	
+	static void arrangeFiles(String folder) {
+		File dir = new File(folder);
+		String msgToUsers;
+		String boxTitle;
+		
+		while (!dir.exists()) {
+			JOptionPane errorPane = new JOptionPane();
+			errorPane.showMessageDialog(null, "Folder not found", "Error", JOptionPane.ERROR_MESSAGE);
+			
+			msgToUsers = "Enter source path here";
+			boxTitle = "Music File Organizer";
+			JOptionPane genOpPane = new JOptionPane();
+			Object result = genOpPane.showInputDialog(null, msgToUsers, boxTitle, JOptionPane.QUESTION_MESSAGE, null, null, null);			
+			dir = new File((String) result);
+		}
+	//	System.out.println(dir.getParentFile());
+		msgToUsers = "Enter name of new folder";
+		boxTitle = "Music File Organizer";
+		JOptionPane genOpPane = new JOptionPane();
+		Object name = genOpPane.showInputDialog(null, msgToUsers, boxTitle, JOptionPane.QUESTION_MESSAGE, null, null, null);
+		String newFolderPath = dir.getParentFile() + "\\" + (String) name;
+		File newFolder = new File(newFolderPath);
+		if (!newFolder.exists()) {
+			newFolder.mkdirs();
+		} else {
+			System.out.println("Already exists");
+		}
+	//	helperIter(dir, newFolder);
+		
+		String destStemStr = dir.getParent() + "\\" + newFolder.getName();
+		helperIter(dir, destStemStr);
+		
+		
+	}
+	
+	static void helperIter(File musicFolder, String destStemStr) {
+		for (File file: musicFolder.listFiles()) {
+			if (!file.isDirectory()) {
+				Path destPath = Paths.get(destStemStr, "\\", file.getName());
+				try {
+					Files.copy(file.toPath(), destPath);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				helperIter(file, destStemStr);
+			}
 		}
 	}
 	
